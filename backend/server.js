f18f261ad4bf8ea3633d809262e422ae063f4d5f
Aditya-Dubey-2002 <79043450+Aditya-Dubey-2002@users.noreply.github.com>
2025-01-09 13:19:43 +0530

    Added documentation page in backend server at /help

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const userRoutes = require('./src/routes/userRoutes.js');
 const orderRoutes = require('./src/routes/orderRoutes.js');
 const stockRoutes = require('./src/routes/stockRoutes.js');
 const holdingsRoutes = require('./src/routes/holdingsRoutes.js');
+const path = require('path');
+// const documentation = require('./src/data/documentation.js');
 dotenv.config();
+
 
 const app = express();
 app.use(cors());
@@ -28,6 +31,19 @@ app.use('/api/stock', stockRoutes);
 app.use('/api/holdings', holdingsRoutes);
 
 // Sync the database and start the server
+app.get('/',(req,res)=>{
+    res.send('Welcome to the StockItUp API ! Go to /help for documentation.');
+})
+
+app.get('/help',(req,res)=>{
+    const filePath = path.join(__dirname, 'documentation.html'); // Path to your HTML file
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            console.error('Error sending file:', err);
+            res.status(500).send('An error occurred while sending the file.');
+        }
+    });
+})
 
 sequelize.sync()
     .then(() => {
